d285e5f256e5ee08ac8b11053e8779d56084e011
Rohit Ware <rohitware99@gmail.com>
2025-04-08 20:12:38 +0530

    string length

---
+++ 1-str-length.js
@@ -0,0 +1,9 @@
+//WAP to find the length or total number of characters of the strings
+const str = "Hello, World!";
+const str1 = "JavaScript is the language of Internet";
+
+strLength = str.length;
+console.log(`length of given string is:`, strLength);
+
+str1Lenght = str1.length;
+console.log(`length of given string is:`, str1Lenght);
