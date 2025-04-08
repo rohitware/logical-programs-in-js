266881d6086c5392c7ca7c7716d8bc03b8a30118
Rohit Ware <rohitware99@gmail.com>
2025-04-08 20:13:34 +0530

    first character of the given strings

---
+++ 3-first-character.js
@@ -0,0 +1,17 @@
+// 3. WAP to print or log the first character of the given strings
+
+function findFirstChar(str) {
+  return str.charAt(0);
+}
+const str1 = `React`;
+const str2 = `Elon Musk`;
+const str3 = `Apple founder is Stew Job`;
+
+let res1 = findFirstChar(str1);
+console.log(res1);
+
+let res2 = findFirstChar(str2);
+console.log(res2);
+
+let res3 = findFirstChar(str3);
+console.log(res3);
