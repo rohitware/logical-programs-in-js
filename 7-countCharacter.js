a00caf9d33828e7e866e8172e0c2f3659685033b
Rohit Ware <rohitware99@gmail.com>
2025-04-09 00:24:14 +0530

    add code for "A" also

--- 7-countCharacter.js
+++ 7-countCharacter.js
@@ -2,16 +2,16 @@ function countCharactor(str) {
     let count = 0;
 
     for (let i = 0; i < str.length; i++) {
-        if (str[i] === 'a') {
+        if (str[i] === 'a'|| str[i] === 'A') {
             count++;
         }
     }
     return count;
 }
-let str1 = "Javascript";
+let str1 = "Javascript A A";
 res = countCharactor(str1);
 console.log(res);
 
-let str2 = "Do or Die";
+let str2 = "Do A or Die";
 res = countCharactor(str2);
 console.log(res);
